522652e37329ad5a854c7bcb0c65048e989406b6
RybOlya <olga9rybenchuk@gmail.com>
2022-09-23 01:40:07 +0300

    Formatiing the code

--- js/index.js
+++ js/index.js
@@ -35,18 +35,18 @@ let formValidation = () => {
 let dwellings = [];
 let image = "";
 let acceptData = () => {
-    dwellings.push({
-      img: image,
-      type: dwellingType.value,
-      title: titleInput.value,
-      description: description.value,
-      location: locationInput.value,
-      price: priceInput.value,
-      floors: floorsInput.value,
-      heating: heatingInput.value,
-    });
-    localStorage.setItem("dwellings", JSON.stringify(dwellings));
-    createDwelling();
+  dwellings.push({
+    img: image,
+    type: dwellingType.value,
+    title: titleInput.value,
+    description: description.value,
+    location: locationInput.value,
+    price: priceInput.value,
+    floors: floorsInput.value,
+    heating: heatingInput.value,
+  });
+  localStorage.setItem("dwellings", JSON.stringify(dwellings));
+  createDwelling();
 };
 
 let resetForm = () => {
@@ -68,9 +68,9 @@ var loadFile = function (event) {
 };
 let createDwelling = () => {
   dwelling.innerHTML = "";
-    dwellings.map((x, y) => {
-      console.log(y);
-      return (dwelling.innerHTML += `
+  dwellings.map((x, y) => {
+    console.log(y);
+    return (dwelling.innerHTML += `
           <div class="card" id="${y}">
               <div class="card__image"><img src= "${x.img}"></div>
               <span class="small text-secondary">${x.type}</span>
@@ -85,19 +85,19 @@ let createDwelling = () => {
                 <i onClick= "buyDwelling(this)" class="fas fa-plus"></i>
               </span>
             </div>`);
-    });
-    resetForm();
+  });
+  resetForm();
 };
 let fin_price = 0;
-let buyDwelling = (e)  => {
+let buyDwelling = (e) => {
   let selectedDwelling = e.parentElement.parentElement;
-  
+
   priceInput.value = selectedDwelling.children[4].innerHTML;
-      fin_price +=parseInt(selectedDwelling.children[4].innerHTML);
-      
-      document.getElementById('total_price').value = fin_price;
-      console.log(fin_price);
-      return (addedDwelling.innerHTML += `
+  fin_price += parseInt(selectedDwelling.children[4].innerHTML);
+
+  document.getElementById("total_price").value = fin_price;
+  console.log(fin_price);
+  return (addedDwelling.innerHTML += `
           <div class="basket" id="${selectedDwelling.id}">
           <span class="dwelling__name fw-bold">${selectedDwelling.children[2].innerHTML}</span></br>
               <span class="price font-monospace ">${selectedDwelling.children[4].innerHTML}</span>
@@ -105,16 +105,15 @@ let buyDwelling = (e)  => {
                 <i onClick ="deleteItem(this);" class="fas fa-trash-alt"></i>
               </span>
             </div>`);
-
 };
 let deleteItem = (e) => {
-  fin_price -=parseInt(e.parentElement.parentElement.children[2].innerHTML);
-  console.log(fin_price); 
-  document.getElementById('total_price').value = fin_price;
-    console.log(e.parentElement.parentElement.children[2].innerHTML); 
-    e.parentElement.parentElement.remove();
+  fin_price -= parseInt(e.parentElement.parentElement.children[2].innerHTML);
+  console.log(fin_price);
+  document.getElementById("total_price").value = fin_price;
+  console.log(e.parentElement.parentElement.children[2].innerHTML);
+  e.parentElement.parentElement.remove();
   addedDwelling.splice(e.parentElement.parentElement.id, 1);
-  
+
   localStorage.setItem("addedDwelling", JSON.stringify(addedDwelling));
 };
 
@@ -153,46 +152,30 @@ function sortDwellings() {
     switchcount = 0;
   list = document.getElementById("dwelling");
   switching = true;
-  // Set the sorting direction to ascending:
   dir = "asc";
-  // Make a loop that will continue until no switching has been done:
   while (switching) {
-    // start by saying: no switching is done:
     switching = false;
     a = list.getElementsByClassName("card");
     b = list.getElementsByClassName("dwelling__name");
-    // Loop through all list-items:
     for (i = 0; i < b.length - 1; i++) {
-      //start by saying there should be no switching:
       shouldSwitch = false;
-      /* check if the next item should switch place with the current item,
-      based on the sorting direction (asc or desc): */
       if (dir == "asc") {
         if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
-          /* if next item is alphabetically lower than current item,
-          mark as a switch and break the loop: */
           shouldSwitch = true;
           break;
         }
       } else if (dir == "desc") {
         if (b[i].innerHTML.toLowerCase() < b[i + 1].innerHTML.toLowerCase()) {
-          /* if next item is alphabetically higher than current item,
-          mark as a switch and break the loop: */
           shouldSwitch = true;
           break;
         }
       }
     }
     if (shouldSwitch) {
-      /* If a switch has been marked, make the switch
-      and mark that a switch has been done: */
       a[i].parentNode.insertBefore(a[i + 1], a[i]);
       switching = true;
-      // Each time a switch is done, increase switchcount by 1:
       switchcount++;
     } else {
-      /* If no switching has been done AND the direction is "asc",
-      set the direction to "desc" and run the while loop again. */
       if (switchcount == 0 && dir == "asc") {
         dir = "desc";
         switching = true;
@@ -206,7 +189,6 @@ function searchFor() {
   filter = input.value.toUpperCase();
   list = document.getElementById("dwelling");
   card = list.getElementsByClassName("card");
-  //b = list.getElementsByClassName("dwelling__name");
   for (i = 0; i < card.length; i++) {
     b = card[i].getElementsByClassName("dwelling__name")[0];
     txtValue = b.textContent || b.innerText;
